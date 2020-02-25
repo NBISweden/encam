317b6271132ec04edd8d93f1a60ba5c029021b24
Dan Rosén <dan.rosen@nbis.se>
2020-02-25 17:22:51 +0100

    Add types with tentative names

--- frontend/main.ts
+++ frontend/main.ts
@@ -1,8 +1,27 @@
-const db = require('./db.json')
+declare const require: Function
 
-console.log(db)
+interface Innermost {
+  expression: number
+  coef: number
+  lower: number
+  upper: number
+  p: number
+}
 
-document.body.innerHTML = `<pre>${JSON.stringify(db, 2, 2)}</pre>`
+type Loc = 'STROMA' | 'TUMOR'
+
+type DB = Record<string, Record<string, Record<Loc, Innermost>>>
+
+interface DBs {
+  by_tumor: DB,
+  by_cell: DB
+}
+
+const db: DBs = require('./db.json')
+
+console && console.log(db)
+
+document.body.innerHTML = `<pre>${JSON.stringify(db, undefined, 2)}</pre>`
 
 declare const module: {hot?: {accept: Function}}
 module.hot && module.hot.accept()
