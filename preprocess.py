424d752f8c1257ef37a2efd3f09ad9011ca56f20
Dan Rosén <dan.rosen@nbis.se>
2020-02-27 11:19:22 +0100

    Make json database just an array of rows

--- preprocess.py
+++ preprocess.py
@@ -1,36 +1,35 @@
 '''
 This code preprocesses made-up-data.csv into a json "database" db.json. The json looks like this:
-    {
-      "by_tumor": {
-        "Colon": {
-          "CD4": {
-            "TUMOR": {
-              "expression": 89.02,
-              "coef": 0.6703,
-              "lower": 0.4474,
-              "upper": 1.004,
-              "p": 0.05242
-            },
-            "STROMA": {
-              "expression": 115.8,
-              "coef": 0.6789,
-              "lower": 0.4429,
-              "upper": 1.041,
-              "p": 0.07548
-            }
-          },
-          "CD4_Treg": {
-            "TUMOR": {
-              "expression": 3.248,
-              "coef": 0.8724,
-              "lower": 0.5743,
-              "upper": 1.325,
-              "p": 0.5222
-            },
-            "STROMA": {
-              "expression": 6.725,
-              "coef": 0.6232,...
-later on there is a section "by_cell" as well.
+    [
+      {
+        "tumor":"Colon",
+        "cell":"CD4",
+        "location":"TUMOR",
+        "cell_full":"CD4_TUMOR",
+        "expression":89.02,
+        "coef":0.6703,
+        "lower":0.4474,
+        "upper":1.004,
+        "p":0.05242
+      },
+      {
+        "tumor":"Colon",
+        "cell":"CD4_Treg",
+        "location":"TUMOR",
+        "cell_full":"CD4_Treg_TUMOR",
+        "expression":3.248,
+        "coef":0.8724,
+        "lower":0.5743,
+        "upper":1.325,
+        "p":0.5222
+      },
+      {
+        "tumor":"Colon",
+        "cell":"CD8",
+        "location":"TUMOR",
+        "cell_full":"CD8_TUMOR",
+        "expression":132.5,
+        "coef":0.777,...
 It uses code from python port of forestplots.R, see forestplots.py and forestplots.ipynb.
 '''
 
@@ -60,8 +59,6 @@ data['E'] = data['Event_last_followup'] == 'Dead'
 tumor_types = uniq(data.cohort)
 
 cell_types = uniq(c for c in data.columns if 'TUMOR' in c or 'STROMA' in c)
-locations = ['TUMOR', 'STROMA']
-cell_types_no_loc = uniq([c.replace('_TUMOR', '').replace('_STROMA', '') for c in cell_types])
 
 def coxph_per_type(dd):
     dd = dd.copy()
@@ -95,55 +92,27 @@ def data_per_type(dd):
     return pd.concat((expression, cox), axis=1)
 
 
-processed = {}
+dfs = []
 for t in tumor_types:
-    d = data_per_type(data[data.cohort == t])
-    # d = d.astype('float16')
-    d = d.applymap(lambda x: float(f'{x:.3e}'))
-    d = d.to_dict(orient='index')
-    processed[t] = {
-        cell: {
-            loc: d[cell + '_' + loc]
-            for loc in locations
-            if cell + '_' + loc in d
-        }
-        for cell in cell_types_no_loc
-    }
+    df = data_per_type(data[data.cohort == t])
+    # df = df.astype('float16')
+    df = df.applymap(lambda x: float(f'{x:.3e}'))
+    cell_full = df.index
+    df.reset_index(drop=True, inplace=True)
+    df.insert(0, 'tumor', t)
+    df.insert(1, 'cell', cell_full.map(lambda x: '_'.join(x.split('_')[0:-1])))
+    df.insert(2, 'location', cell_full.map(lambda x: x.split('_')[-1]))
+    df.insert(3, 'cell_full', cell_full)
+    dfs.append(df)
 
-
-def transpose(d):
-    assert all(isinstance(v, dict) for v in d.values())
-    outer_keys = d.keys()
-    inner_keys = uniq(k for v in d.values() for k in v.keys())
-    return {
-        ik: {
-            ok: d[ok][ik]
-            for ok in outer_keys
-            if ik in d[ok]
-        }
-        for ik in inner_keys
-    }
-
-A = {'a': {'x': 1, 'y': 2}, 'b': {'y': 3, 'z': 4}}
-B = {'x': {'a': 1}, 'y': {'a': 2, 'b': 3}, 'z': {'b': 4}}
-
-json_eq = lambda x, y: json.dumps(x, sort_keys=True) == json.dumps(y, sort_keys=True)
-
-assert json_eq(transpose(A), B)
-assert json_eq(transpose(transpose(A)), A)
-
-result = {
-    'by_tumor': processed,
-    'by_cell': transpose(processed)
-}
-
-result_json = json.dumps(result, indent=2)
+db = pd.concat(dfs, axis=0).reset_index(drop=True)
+db_str = db.to_json(orient='records', indent=2)
 
 with open('./frontend/db.json', 'w') as fp:
-    fp.write(result_json)
+    fp.write(db_str)
 
 import gzip
-print('\n'.join(result_json.split('\n')[:30]) + '...')
+print('\n'.join(db_str.split('\n')[:30]) + '...')
 print()
-print('json len:', len(result_json))
-print('gzipped:', len(gzip.compress(result_json.encode())))
+print('json len:', len(db_str))
+print('gzipped:', len(gzip.compress(db_str.encode())))
