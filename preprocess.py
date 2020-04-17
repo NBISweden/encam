c40b7cb97f7b258e2917044163ad59ba4a7e0267
Dan Rosén <dan.rosen@nbis.se>
2020-04-17 15:03:22 +0200

    Update preprocess script

--- preprocess.py
+++ preprocess.py
@@ -1,36 +1,51 @@
 '''
-This code preprocesses made-up-data.csv into a json "database" db.json. The json looks like this:
-    [
-      {
-        "tumor":"Colon",
-        "cell":"CD4",
-        "location":"TUMOR",
-        "cell_full":"CD4_TUMOR",
-        "expression":89.02,
-        "coef":0.6703,
-        "lower":0.4474,
-        "upper":1.004,
-        "p":0.05242
-      },
-      {
-        "tumor":"Colon",
-        "cell":"CD4_Treg",
-        "location":"TUMOR",
-        "cell_full":"CD4_Treg_TUMOR",
-        "expression":3.248,
-        "coef":0.8724,
-        "lower":0.5743,
-        "upper":1.325,
-        "p":0.5222
-      },
-      {
-        "tumor":"Colon",
-        "cell":"CD8",
-        "location":"TUMOR",
-        "cell_full":"CD8_TUMOR",
-        "expression":132.5,
-        "coef":0.777,...
-It uses code from python port of forestplots.R, see forestplots.py and forestplots.ipynb.
+    This code preprocesses SIM.csv into a json "database" frontend/db.json. The json looks like this:
+
+        [
+          {
+            "tumor":"Colon",
+            "cell":"CD4",
+            "location":"TUMOR",
+            "cell_full":"CD4_TUMOR",
+            "expression":89.02,
+            "coef":0.6703,
+            "lower":0.4474,
+            "upper":1.004,
+            "p":0.05242
+          },
+          {
+            "tumor":"Colon",
+            "cell":"CD4_Treg",
+            "location":"TUMOR",
+            "cell_full":"CD4_Treg_TUMOR",
+            "expression":3.248,
+            "coef":0.8724,
+            "lower":0.5743,
+            "upper":1.325,
+            "p":0.5222
+          },
+          {
+            "tumor":"Colon",
+            "cell":"CD8",
+            "location":"TUMOR",
+            "cell_full":"CD8_TUMOR",
+            "expression":132.5,
+            "coef":0.777,
+            ...
+          },
+          ...
+        ]
+
+    It uses code from python port of forestplots.R, see forestplots.py and forestplots.ipynb.
+
+    Additionally it creates frotend/codes.json which is a mapping from tumor type acronyms
+    to their expanded versions:
+
+        {
+          "COAD": "Colon adenocarcinoma",
+          "READ": "Rectum adenocarcinoma",
+          ...
+        }
 '''
 # !pip install --user lifelines
 from lifelines import CoxPHFitter
@@ -48,8 +63,11 @@ def uniq(xs):
         if x not in seen
     ]
 
-data = pd.read_csv("./made-up-data.csv")
-data = data[ data['PreOp_treatment_yesno'] == 'No' ]
+data = pd.read_csv("./SIM.csv")
+
+# Whitespace stripping because of some trailing Morphological_type spaces
+strip = lambda x: x.strip() if isinstance(x, str) else x
+data = data.applymap(strip)
 
 data['T'] = data['Time_Diagnosis_Last_followup']
 data['E'] = data['Event_last_followup'] == 'Dead'
@@ -69,7 +87,7 @@ def coxph_per_type(dd):
         dd_c = dd[[c, 'T', 'E']]
         dd_c = dd_c[~pd.isnull(dd_c).any(axis=1)]
         cph = CoxPHFitter()
-        cph.fit(dd_c, 'T', event_col='E')
+        cph.fit(dd_c, 'T', event_col='E') # fits are ~15-60 ms each
         univariate_results.append(cph.summary)
 
     cox = pd.concat(univariate_results)
@@ -90,9 +108,13 @@ def data_per_type(dd):
     return pd.concat((expression, cox), axis=1)
 
 
+# This takes about 15s
 dfs = []
 for t in tumor_types:
-    df = data_per_type(data[data.Tumor_type_code == t])
+    df = data_per_type(data[
+        (data.Tumor_type_code == t) &
+        (data['PreOp_treatment_yesno'] == 'No')
+    ])
     # df = df.astype('float16')
     df = df.applymap(lambda x: float(f'{x:.3e}'))
     cell_full = df.index
@@ -120,4 +142,69 @@ codes_dict = {d['Tumor_type_code']: d['Tumor_type'] for d in codes_list}
 
 with open('./frontend/codes.json', 'w') as fp:
     json.dump(codes_dict, fp)
+
+
+def form_configuration():
+    '''
+    This is a work in progress of getting the configuration for the form.
+    '''
+
+    def tidy_values(values):
+        values = uniq(values)
+        values = sorted(values, key=lambda x: (isinstance(x, float), x))
+        values = [ 'missing' if pd.isnull(v) else v for v in values ]
+        return values
+
+    tumor_specific_columns = [
+        'Anatomical_location',
+        'Morphological_type',
+        'MSI_ARTUR',
+    ]
+
+    tumor_specific_values = []
+    for column in tumor_specific_column:
+        # values = uniq(data[c][lambda x: ~pd.isnull(x)])
+        # print(c, values)
+        for tumor in tumor_types:
+            values = tidy_values(data[data.Tumor_type_code == tumor][column])
+            if len(values) > 1:
+                tumor_specific_values.append({
+                    'column': column,
+                    'tumor': tumor,
+                    'values': values
+                })
+
+    variant_columns = [
+        # 'Gender',
+        # 'Anatomical_location',
+        # 'Morphological_type',
+        'clinical_stage',
+        'pT_stage',
+        'pN_stage',
+        'pM_stage',
+        'Diff_grade',
+        'Neuralinv',
+        'Vascinv',
+        'PreOp_treatment_yesno',
+        'PostOp_type_treatment',
+        # 'MSI_ARTUR',
+    ]
+
+    variant_values = []
+    for column in variant_columns:
+        values = tidy_values(data[column])
+        variant_values.append({
+            'column': column,
+            'values': values
+        })
+
+    config = {
+        'variant_values': variant_values,
+        'tumor_specific_values': tumor_specific_values,
+    }
+    from pprint import pprint
+    pprint(config)
+    # print(json.dumps(config, indent=2))
+
+form_configuration()
 
