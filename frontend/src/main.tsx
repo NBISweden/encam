2b2fde75fa3a65d201ba0ee694e15898b3a20396
Dan Rosén <dan.rosen@nbis.se>
2020-06-11 16:07:45 +0200

    Rename views to Views

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,7 @@ module.hot && module.hot.accept()
 
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-import {Views} from './views'
+import {Views} from './Views'
 import {CssBaseline} from '@material-ui/core'
 
 const root = document.querySelector('#root')
