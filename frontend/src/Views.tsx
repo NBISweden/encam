f266443f3c9fcfd392144473d8df4f78dcbe082f
Dan Rosén <dan.rosen@nbis.se>
2020-06-17 18:57:26 +0200

    Rearrange order of tabs

--- frontend/src/Views.tsx
+++ frontend/src/Views.tsx
@@ -44,14 +44,14 @@ export function Views() {
   const [tab, set_tab] = React.useState(0)
   const tabs = [
     {label: 'Splash',                component: <Splash/>},
-    {label: 'Splash mock backend',   component: <Splash key="mock" backend={splash_test_backend}/>},
+    {label: 'Form&Plot',             component: <FormAndBoxPlot key="Form"/>},
+    {label: 'Group Form&Plot',       component: <FormAndBoxPlot key="TwoForms" form={form.TwoForms}/>},
     {label: 'Boxplot',               component: <ui.InlinePaper><VegaBoxplot data={boxplot_test_data} options={{facet:"cell", inner:["tumor", "group", "location"], color: ["tumor", "group"]}}/></ui.InlinePaper>},
     {label: 'Kaplan-Meier plot',     component: <ui.InlinePaper><VegaKMPlot points={kmplot_test_data.points}/></ui.InlinePaper>},
     {label: 'Boxplot with Controls', component: <ui.InlinePaper><BoxplotWithControls data={boxplot_test_data} facet="cell"/></ui.InlinePaper>},
     {label: 'Form',                  component: <ui.InlinePaper><form.Form     conf={form_test_conf}/></ui.InlinePaper>},
     {label: 'Group Form',            component: <ui.InlinePaper><form.TwoForms conf={form_test_conf}/></ui.InlinePaper>},
-    {label: 'Form&Plot',             component: <FormAndBoxPlot key="Form"/>},
-    {label: 'Group Form&Plot',       component: <FormAndBoxPlot key="TwoForms" form={form.TwoForms}/>},
+    {label: 'Splash mock backend',   component: <Splash key="mock" backend={splash_test_backend}/>},
     {label: 'Domplots demo',         component: <ui.InlinePaper><domplots.Demo/></ui.InlinePaper>},
   ]
   ui.useKeydown(e => {
