f0eba6c2c1cc8125a84d356fa04c61c94de273b9
Dan Rosén <dan.rosen@nbis.se>
2020-06-18 12:02:37 +0200

    [deploy] Only remove forward secrecy part of TLS (for now)

--- system/entrypoint.sh
+++ system/entrypoint.sh
@@ -1,5 +1,26 @@
 #!/bin/sh
 
+# Set up self signed certificate in case none is supplied.
+
+yes '' | openssl req -x509 -nodes -days 365 -newkey rsa:4096 -keyout /code/key.pem -out /code/cert.pem > /dev/null 2>/dev/null
+cp /code/cert.pem /code/ca.pem
+
+for p in {cert,key,ca}; do
+  if [ -r /config/$p.pem ]; then
+    echo "$p.pem supplied, copying"
+    cp "/config/$p.pem" /code/
+  fi
+done
+
+if [ -r /config/dhparam.pem ]; then
+    echo "!!! Skipping pre-generated forward secrecy crypto parameters"
+else
+    echo "!!! Skipping generating forward secrecy crypto parameters"
+    # openssl dhparam -out /code/dhparam.pem 2048 > /dev/null 2>/dev/null
+fi
+
+chown www-data.www-data /code/*.pem
+
 if getent hosts encam-frontend-devel >/dev/null; then
     # Development mode
     mv /etc/nginx/encam.devel /etc/nginx/encam
