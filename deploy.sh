b5754e81910cf8cf2d6415e8de6ad41b10f303a5
Dan Rosén <dan.rosen@nbis.se>
2020-06-18 11:50:05 +0200

    [deploy] Improve deploy.sh slightly

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,13 @@
-#!/bin/sh
-test $VERSION || {
+#!/bin/sh -xe
+test -n "$VERSION" || {
     echo '$VERSION needs to be set';
     exit 1
 }
 echo "export const version = '$VERSION'" > frontend/src/version.tsx
+function cleanup {
+    echo "export const version = 'git'" > frontend/src/version.tsx
+}
+trap cleanup EXIT
 TAG="docker.pkg.github.com/nbisweden/encam/test:$VERSION"
 docker-compose build
 docker tag encam_main "$TAG"
@@ -12,4 +16,3 @@ ssh ubuntu@130.238.28.162 '
     docker stop $(docker ps --quiet);
     docker run --detach -p 80:8080 -p 443:8443 '"$TAG"'
 '
-echo "export const version = 'git'" > frontend/src/version.tsx
