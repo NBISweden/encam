d08dad87192880aeda90629533957db4e5c28e71
Dan Rosén <dan.rosen@nbis.se>
2020-08-14 14:19:01 +0200

    Fix binning-related issues in FormAndKMPlot, #59

--- frontend/src/FormAndPlot.tsx
+++ frontend/src/FormAndPlot.tsx
@@ -7,7 +7,7 @@ import * as utils from './utils'
 
 import {BoxplotWithControls} from './BoxplotWithControls'
 import {VegaKMPlot, Points} from './VegaKMPlot'
-import {VegaCumulativeCount, cucount, Row} from './VegaCumulativeCount'
+import {VegaCumulativeCount, cucount, slider_max, bin_sizes, Row} from './VegaCumulativeCount'
 import * as form from './Form'
 
 import {CircularProgress} from '@material-ui/core'
@@ -108,88 +108,95 @@ export function KMPlotWithControls({filter = undefined as undefined | Record<str
 
   const [cutoffs, set_cutoffs] = React.useState([] as number[])
 
-  const cu_max = (cu_data: Row[]) => cu_data.slice(-1)[0].cucount
-
   ui.useAsync(async () => {
+    // Get the expression levels when filter or location changes
     if (filter) {
       const filter_full = {
         ...filter,
-        group_sizes: null,
         cell_full: filter.cell + '_' + location.toUpperCase(),
-        // num_groups,
       }
       console.time('expression')
-      const next_expr_data = await request('expression', filter_full)
+      const next_expr_data = [...(await request('expression', filter_full))]
       console.timeEnd('expression')
       ReactDOM.unstable_batchedUpdates(() => {
         set_expr_data(next_expr_data)
-        if (!expr_data || expr_data.length != next_expr_data) {
+        if (!expr_data || expr_data.length !== next_expr_data.length) {
           set_cutoffs([])
         }
       })
     }
   }, [filter, location])
 
+  const cu_data = React.useMemo(() => expr_data && cucount(expr_data, cutoffs), [
+    expr_data,
+    cutoffs,
+  ])
+
   React.useLayoutEffect(() => {
-    if (expr_data) {
-      const cu_data = cucount(expr_data, cutoffs)
-      const max = cu_max(cu_data)
-      const cutoff = (i: number) => {
-        const r = (i + 1) / num_groups
-        return Math.round(r * max)
-      }
-      const next_cutoffs = utils.enumTo(num_groups - 1).map(cutoff)
-      if (!cu_data || num_groups - 1 != cutoffs.length) {
+    // update cutoffs if num_groups or the data was changed
+    if (cu_data) {
+      const wrong_length = num_groups - 1 != cutoffs.length
+      const too_large = cutoffs.some(c => c > slider_max(cu_data))
+      if (wrong_length || too_large) {
+        const cutoff = (i: number) => {
+          const r = (i + 1) / num_groups
+          return Math.round(r * slider_max(cu_data))
+        }
+        const next_cutoffs = utils.enumTo(num_groups - 1).map(cutoff)
         set_cutoffs(next_cutoffs)
       }
     }
-  }, [expr_data, num_groups])
+  }, [cu_data, num_groups])
 
   ui.useDebounce(
     400,
     React.useCallback(async () => {
-      if (filter && cutoffs.length && expr_data) {
-        const cu_data = cucount(expr_data, cutoffs)
-        set_loading(true)
-        const cutoffs_sorted = [...cutoffs.sort(utils.by(x => x)), cu_max(cu_data)]
-        // [100, 120, max]
-        // [0 - 100, 120 - 20, max - 120]
-        const group_sizes = cutoffs_sorted.map((v, i, a) => v - (a[i - 1] || 0))
-        console.time('survival')
-        console.log(cutoffs_sorted)
+      // after a little while of no changes to the slider request new survival calculation
+      if (filter && cu_data) {
+        const group_sizes = bin_sizes(cu_data)
         console.log(group_sizes)
         const filter_full = {
           ...filter,
-          group_sizes,
           cell_full: filter.cell + '_' + location.toUpperCase(),
+          group_sizes,
           num_groups: group_sizes.length,
         }
         let res = undefined as any
         try {
+          console.time('survival')
+          set_loading(true)
           res = await request('survival', {
             ...filter_full,
             group_sizes,
           })
         } catch {
           ReactDOM.unstable_batchedUpdates(() => {
+            console.timeEnd('survival')
             set_loading(false)
             set_plot_data(undefined)
           })
           return
         }
-        console.timeEnd('survival')
-        console.log({res})
         ReactDOM.unstable_batchedUpdates(() => {
+          console.timeEnd('survival')
           set_loading(false)
           set_plot_data(res.points)
         })
       } else {
         set_loading(false)
       }
-    }, [filter, location, utils.str(cutoffs)])
+    }, [filter, location, cu_data])
   )
 
-  const cu_data = expr_data && cucount(expr_data, cutoffs)
+  ui.useWhyChanged(KMPlotWithControls, {
+    filter,
+    plot_data,
+    loading,
+    location,
+    num_groups,
+    cutoffs,
+    cu_data,
+  })
 
   const classes = useStyles()
   const plot = (plot_data || cu_data) && (
@@ -206,8 +213,8 @@ export function KMPlotWithControls({filter = undefined as undefined | Record<str
             <Slider
               style={{marginTop: 30}}
               aria-labelledby="cutoffs-slider"
-              min={0}
-              max={cu_max(cu_data)}
+              min={1}
+              max={slider_max(cu_data)}
               value={cutoffs}
               onChange={(_, vs) => Array.isArray(vs) && set_cutoffs(vs)}
               valueLabelDisplay="on"
@@ -219,7 +226,6 @@ export function KMPlotWithControls({filter = undefined as undefined | Record<str
     </div>
   )
 
-  ui.useWhyChanged(KMPlotWithControls, {filter, plot_data, loading, location, num_groups})
   return <LoadingPlot loading={loading} plot={plot} />
 }
 
